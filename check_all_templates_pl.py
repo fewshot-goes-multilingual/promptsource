d7c8000e368321d1247f46d49a380cea652dce99
Piotr Gramacki <p.gramacki@gmail.com>
2023-02-26 01:26:49 +0100

    add NER dataset

--- check_all_templates_pl.py
+++ check_all_templates_pl.py
@@ -28,4 +28,57 @@ templates = DatasetTemplates("fewshot-goes-multilingual/pl_klej-cdsc-e")
 
 verbalize_all_templates(dataset[1], templates)
 
+# NER
+dataset = load_dataset("laugustyniak/political-advertising-pl", split="train")
+names = dataset.info.features["tags"].feature.names
+dataset = dataset.map(lambda x: {"tag_names": [names[i] for i in x["tags"]]})
+
+samples = []
+for item in dataset:
+    text = " ".join(item["tokens"])
+    seen_entities = []
+    for idx, tag in enumerate(item["tag_names"]):
+        if tag != "O":
+            category = tag.split("-")[1]
+            position = tag.split("-")[0]
+            if position == "B":
+                seen_entities.append({
+                    "text": text,
+                    "label_type": category,
+                    "label": item["tokens"][idx],
+                })
+            elif position == "I":
+                seen_entities[-1]["label"] += " " + item["tokens"][idx]
+    # remove duplicate categories
+    seen_categories = set()
+    duplicated_categories = set()
+
+    for entity in seen_entities:
+        if entity["label_type"] in seen_categories:
+            duplicated_categories.add(entity["label_type"])
+        else:
+            seen_categories.add(entity["label_type"])
+    seen_entities = [e for e in seen_entities if e["label_type"] not in duplicated_categories]
+
+    samples.extend(seen_entities)
+
+translations = {
+    'DEFENSE_AND_SECURITY': "Obronność i bezpieczeństwo",
+    'EDUCATION': "Edukacja",
+    'FOREIGN_POLICY': "Polityka zagraniczna",
+    'HEALHCARE': "Służba zdrowia",
+    'IMMIGRATION': "Imigracja",
+    'INFRASTRUCTURE_AND_ENVIROMENT': "Infrastruktura i środowisko",
+    'POLITICAL_AND_LEGAL_SYSTEM': "System polityczny i prawny",
+    'SOCIETY': "Społeczeństwo",
+    'WELFARE': "Dobrobyt",
+}
+
+dataset_flat = Dataset.from_list(samples)
+dataset_flat = dataset_flat.map(lambda x: {"label_type_pl": translations[x["label_type"]]})
+
+templates = DatasetTemplates("fewshot-goes-multilingual/pl_political-advertising-pl")
+
+verbalize_all_templates(dataset_flat[1], templates)
+
 print("done")
