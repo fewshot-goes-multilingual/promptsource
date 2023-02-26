8f995de2939fdfda95ad18776692d56661bc42cd
Piotr Gramacki <p.gramacki@gmail.com>
2023-02-26 19:45:15 +0100

    extract translations to utils file and ner processing to function

--- check_all_templates_pl.py
+++ check_all_templates_pl.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset, Dataset
 from promptsource.templates import DatasetTemplates
 from typing import Dict, Any
+from utils_pl import political_advertasing_translations
 
 
 def verbalize_all_templates(sample: Dict[str, Any], all_templates: DatasetTemplates) -> None:
@@ -9,6 +10,43 @@ def verbalize_all_templates(sample: Dict[str, Any], all_templates: DatasetTempla
         result = template.apply(sample)
         print("INPUT: ", result[0])
         print("OUTPUT: ", result[1])
+
+def process_ner_dataset(dataset: Dataset, translations: Dict[str, str], output_column: str) -> Dataset:
+    names = dataset.info.features[output_column].feature.names
+    dataset = dataset.map(lambda x: {"tag_names": [names[i] for i in x[output_column]]})
+
+    samples = []
+    for item in dataset:
+        text = " ".join(item["tokens"])
+        seen_entities = []
+        for idx, tag in enumerate(item["tag_names"]):
+            if tag != "O":
+                category = tag.split("-")[1]
+                position = tag.split("-")[0]
+                if position == "B":
+                    seen_entities.append({
+                        "text": text,
+                        "label_type": category,
+                        "label": item["tokens"][idx],
+                    })
+                elif position == "I":
+                    seen_entities[-1]["label"] += " " + item["tokens"][idx]
+        # remove duplicate categories
+        seen_categories = set()
+        duplicated_categories = set()
+
+        for entity in seen_entities:
+            if entity["label_type"] in seen_categories:
+                duplicated_categories.add(entity["label_type"])
+            else:
+                seen_categories.add(entity["label_type"])
+        seen_entities = [e for e in seen_entities if e["label_type"] not in duplicated_categories]
+
+        samples.extend(seen_entities)
+
+    dataset_flat = Dataset.from_list(samples)
+    dataset_flat = dataset_flat.map(lambda x: {"label_type_pl": translations[x["label_type"]]})
+    return dataset_flat
 
 # Sentiment
 dataset = load_dataset("clarin-pl/polemo2-official", split="test")
@@ -30,55 +68,9 @@ verbalize_all_templates(dataset[1], templates)
 
 # NER
 dataset = load_dataset("laugustyniak/political-advertising-pl", split="train")
-names = dataset.info.features["tags"].feature.names
-dataset = dataset.map(lambda x: {"tag_names": [names[i] for i in x["tags"]]})
-
-samples = []
-for item in dataset:
-    text = " ".join(item["tokens"])
-    seen_entities = []
-    for idx, tag in enumerate(item["tag_names"]):
-        if tag != "O":
-            category = tag.split("-")[1]
-            position = tag.split("-")[0]
-            if position == "B":
-                seen_entities.append({
-                    "text": text,
-                    "label_type": category,
-                    "label": item["tokens"][idx],
-                })
-            elif position == "I":
-                seen_entities[-1]["label"] += " " + item["tokens"][idx]
-    # remove duplicate categories
-    seen_categories = set()
-    duplicated_categories = set()
-
-    for entity in seen_entities:
-        if entity["label_type"] in seen_categories:
-            duplicated_categories.add(entity["label_type"])
-        else:
-            seen_categories.add(entity["label_type"])
-    seen_entities = [e for e in seen_entities if e["label_type"] not in duplicated_categories]
-
-    samples.extend(seen_entities)
-
-translations = {
-    'DEFENSE_AND_SECURITY': "Obronność i bezpieczeństwo",
-    'EDUCATION': "Edukacja",
-    'FOREIGN_POLICY': "Polityka zagraniczna",
-    'HEALHCARE': "Służba zdrowia",
-    'IMMIGRATION': "Imigracja",
-    'INFRASTRUCTURE_AND_ENVIROMENT': "Infrastruktura i środowisko",
-    'POLITICAL_AND_LEGAL_SYSTEM': "System polityczny i prawny",
-    'SOCIETY': "Społeczeństwo",
-    'WELFARE': "Dobrobyt",
-}
-
-dataset_flat = Dataset.from_list(samples)
-dataset_flat = dataset_flat.map(lambda x: {"label_type_pl": translations[x["label_type"]]})
-
+dataset_processed = process_ner_dataset(dataset, political_advertasing_translations, "tags")
 templates = DatasetTemplates("fewshot-goes-multilingual/pl_political-advertising-pl")
 
-verbalize_all_templates(dataset_flat[1], templates)
+verbalize_all_templates(dataset_processed[1], templates)
 
 print("done")
---
+++ utils_pl.py
@@ -0,0 +1,11 @@
+political_advertasing_translations = {
+    'DEFENSE_AND_SECURITY': "Obronność i bezpieczeństwo",
+    'EDUCATION': "Edukacja",
+    'FOREIGN_POLICY': "Polityka zagraniczna",
+    'HEALHCARE': "Służba zdrowia",
+    'IMMIGRATION': "Imigracja",
+    'INFRASTRUCTURE_AND_ENVIROMENT': "Infrastruktura i środowisko",
+    'POLITICAL_AND_LEGAL_SYSTEM': "System polityczny i prawny",
+    'SOCIETY': "Społeczeństwo",
+    'WELFARE': "Dobrobyt",
+}
